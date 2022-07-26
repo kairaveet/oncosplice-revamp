d063320cdabbe7d988260aa3e0d383e6f8a30205
DanSchnell <38895089+DanSchnell@users.noreply.github.com>
2022-07-26 09:39:49 -0400

    Add files via upload

--- IDKfunction1.py
+++ IDKfunction1.py
@@ -3,22 +3,24 @@
 # 
 #  IDK function 
 #    
-# July 25, 2022
+# July 26, 2022
 #
-#  Can identify K using 1 or more of the following methods:
+#  Can identify Intrinsic Dimension K of numeric matrix using 1 or more of the following methods:
 #   MADA, CORRINT, MOM, PCAF0, TLE
+# 
 # **** IDK function *******
+#
 def IDK( PSIdf, Kmethods ):
-#   Set up pandas series for estimates of K.  -1 for not done or error.
-#   PSIdf is a pandas data frame with events (features) as columns
+#   PSIdf is a pandas data frame with events (features) as columns, no missing values.
 #   event names are not needed; can be row names but not a column.
 #   Kmethods is a pandas series with True/False for each available method.
 #   Available methods are: MADA, CORRINT, MOM, PCAF0, TLE.
 #
     import skdim
     import numpy as np
-    import pandas as pd 
-
+    import pandas as pd
+# 
+#   Set up pandas series for estimates of K.  NaN for not done or error.
     Kests={ 'MADA' : np.nan, 'CORRINT' : np.nan, 'MOM' : np.nan , 'PCAF0' : np.nan, 'TLE' : np.nan }
 #
     Kests = pd.Series(Kests)
